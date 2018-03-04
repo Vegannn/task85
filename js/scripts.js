b40d62527582a505b501bd70ae07effab3d75da2
Rafal Stefaniak <vegecia@gmail.com>
2018-03-04 18:52:14 +0100

    task 85

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // scripts.js
 
-var a = 8;
-var b = 10;
+var a = prompt('Enter base of triangle');
+var h = prompt('Enter height of triangle');
 var triangleArea = a*h/2;
+alert('Triangle field is equal to: ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
